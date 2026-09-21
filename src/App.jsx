2d367b96786a77a863c56ef9ca4f6644d7b76bca
Manuel Var Ale <manuel.vargas@compatai.mx>
2026-09-20 19:11:03 -0600

    Remove Demo and Store routes from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home  from './pages/Home'
-import Demo  from './pages/Demo'
-import Store from './pages/Store'
 
 function getSubdomain() {
   const host = window.location.hostname
@@ -37,8 +35,6 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/"      element={<Home />}  />
-        <Route path="/demo"  element={<Demo />}  />
-        <Route path="/store" element={<Store />} />
         <Route path="*"      element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
